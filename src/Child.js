4133717cbcb49987c48be601b3cbaed1bdbb3783
pbsmith82 <pbsmith82@gmail.com>
2021-05-19 16:55:06 -0400

    commit tree bc7419f4783b3760c74f20b0436fca20f2de7a3b

--- src/Child.js
+++ src/Child.js
@@ -5,8 +5,9 @@ class Child extends Component {
   render() {
     return (
       <div
-        className="child"
-        style={{backgroundColor: "#FFF"}}
+      onClick={ () => this.props.handleColorChange(getRandomColor())}
+      className="child"
+      style={{backgroundColor: this.props.color}}
       ></div>
     )
   }
